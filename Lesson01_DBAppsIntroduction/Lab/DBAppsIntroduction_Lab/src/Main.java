6ce1c62975a2a35127b345267b5f0dc5cc30f57a
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-18 17:11:50 +0200

    Update Main.java

--- Lesson01_DBAppsIntroduction/Lab/DBAppsIntroduction_Lab/src/Main.java
+++ Lesson01_DBAppsIntroduction/Lab/DBAppsIntroduction_Lab/src/Main.java
@@ -1,5 +1,4 @@
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -15,9 +14,23 @@ public class Main {
         Properties credentials = new Properties();
         credentials.setProperty("user", username);
         credentials.setProperty("password", password);
-        DriverManager.getConnection
+        Connection connection = DriverManager.getConnection
                 ("jdbc:mysql://localhost:3306/soft_uni", credentials);
+
         // Execute Query
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("SELECT * FROM employees WHERE salary > ? LIMIT 10");
+        preparedStatement.setDouble(1, 17000);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
         // Print Result
+        while (resultSet.next()){
+           long id = resultSet.getLong("employee_id");
+           String first_name = resultSet.getString("first_name");
+           double salary = resultSet.getDouble("salary");
+           Timestamp hireDate = resultSet.getTimestamp("hire_date");
+
+            System.out.printf("%d - %s %.2f %s%n", id, first_name, salary, hireDate);
+        }
     }
 }
