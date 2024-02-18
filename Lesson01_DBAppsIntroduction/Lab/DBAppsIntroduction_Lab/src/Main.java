488c711ac2406212982ec4efee9b94d2874041a7
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-18 17:40:29 +0200

    Update Main.java

--- Lesson01_DBAppsIntroduction/Lab/DBAppsIntroduction_Lab/src/Main.java
+++ Lesson01_DBAppsIntroduction/Lab/DBAppsIntroduction_Lab/src/Main.java
@@ -20,16 +20,26 @@ public class Main {
         // Execute Query
         PreparedStatement preparedStatement =
                 connection.prepareStatement("SELECT * FROM employees WHERE salary > ? LIMIT 10");
-
         preparedStatement.setDouble(1, 17000);
         ResultSet resultSet = preparedStatement.executeQuery();
 
+        //connection.setAutoCommit(false);
+        PreparedStatement updateStatement =
+                connection.prepareStatement("UPDATE employees SET first_name = ? WHERE employee_id = ?");
+        updateStatement.setString(1, "Changed Again");
+        updateStatement.setInt(2,3);
+        int updateResult = updateStatement.executeUpdate();
+        // if (updateResult > 1){
+        //    connection.rollback();
+        //}
+        System.out.println(updateResult);
+
         // Print Result
-        while (resultSet.next()){
-           long id = resultSet.getLong("employee_id");
-           String first_name = resultSet.getString("first_name");
-           double salary = resultSet.getDouble("salary");
-           Timestamp hireDate = resultSet.getTimestamp("hire_date");
+        while (resultSet.next()) {
+            long id = resultSet.getLong("employee_id");
+            String first_name = resultSet.getString("first_name");
+            double salary = resultSet.getDouble("salary");
+            Timestamp hireDate = resultSet.getTimestamp("hire_date");
 
             System.out.printf("%d - %s %.2f %s%n", id, first_name, salary, hireDate);
         }
