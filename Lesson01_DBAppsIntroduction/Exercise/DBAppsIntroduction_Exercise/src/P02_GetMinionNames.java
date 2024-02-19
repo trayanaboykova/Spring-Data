ffc5b88be33ea240400fb6adb2a141f8e172758a
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-19 16:21:30 +0200

    Update P02_GetMinionNames.java

--- Lesson01_DBAppsIntroduction/Exercise/DBAppsIntroduction_Exercise/src/P02_GetMinionNames.java
+++ Lesson01_DBAppsIntroduction/Exercise/DBAppsIntroduction_Exercise/src/P02_GetMinionNames.java
@@ -1,31 +1,51 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.util.Scanner;
 
 public class P02_GetMinionNames {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) throws SQLException {
-        // Connect to DB
         Scanner scanner = new Scanner(System.in);
         System.out.print("Username: ");
         String username = scanner.nextLine();
         System.out.print("Password: ");
         String password = scanner.nextLine();
 
-        Properties properties = new Properties();
-        properties.setProperty("user", username);
-        properties.setProperty("password", password);
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", properties);
+        DBTools dbTools = new DBTools(username, password, "minions_db");
 
         // Statement logic -> create query
-        String query = "";
-        ResultSet resultSet = connection.createStatement().executeQuery(query);
+        int villainId = Integer.parseInt(SCANNER.nextLine());
 
-        // Query output
+
+        String query = "SELECT name FROM villains WHERE id = ?";
+
+        PreparedStatement preparedStatement = dbTools.getConnection().prepareStatement(query);
+        preparedStatement.setInt(1, villainId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (!resultSet.next()) {
+            System.out.printf("No villain with ID %d exists in the database.", villainId);
+            return;
+        }
+        String villainName = resultSet.getString("name");
+        System.out.printf("Villain: %s%n", villainName);
+
+        query = "SELECT m.name, m.age FROM minions m " +
+                "JOIN minions_villains mv on m.id = mv.minion_id " +
+                "WHERE villain_id = ?";
+
+        preparedStatement = dbTools.getConnection().prepareStatement(query);
+        preparedStatement.setInt(1, villainId);
+        resultSet = preparedStatement.executeQuery();
+
+        int index = 0;
         while (resultSet.next()) {
-            System.out.printf("%s %d", resultSet.getString(1), resultSet.getInt(2)).append(System.lineSeparator());
+            System.out.printf("%d. %s %d",
+                    ++index, resultSet.getString("name"),
+                    resultSet.getInt("age"))
+                    .append(System.lineSeparator());
         }
     }
 }
