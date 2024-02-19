e6f5f7c7786a086d648c3ba92d14b357aa8a610b
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-19 14:59:51 +0200

    Update DBTools.java

--- Lesson01_DBAppsIntroduction/Exercise/DBAppsIntroduction_Exercise/src/DBTools.java
+++ Lesson01_DBAppsIntroduction/Exercise/DBAppsIntroduction_Exercise/src/DBTools.java
@@ -1,6 +1,24 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class DBTools {
+    private final String url = "jdbc:mysql://localhost:3306/";
     private Connection connection;
+    public DBTools(String username, String password, String tableName) throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", username);
+        properties.setProperty("password", password);
 
+        try{
+            connection = DriverManager.getConnection(url + tableName, properties);
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public Connection getConnection(){
+        return connection;
+    }
 }
