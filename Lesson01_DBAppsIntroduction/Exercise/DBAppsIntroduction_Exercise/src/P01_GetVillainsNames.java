f0e524fe217aecc5e1a17ae679bd1b18d3d50f85
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-19 14:32:11 +0200

    Update P01_GetVillainsNames.java

--- Lesson01_DBAppsIntroduction/Exercise/DBAppsIntroduction_Exercise/src/P01_GetVillainsNames.java
+++ Lesson01_DBAppsIntroduction/Exercise/DBAppsIntroduction_Exercise/src/P01_GetVillainsNames.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 
 public class P01_GetVillainsNames {
     public static void main(String[] args) throws SQLException {
+        // Connect to DB
         Scanner scanner = new Scanner(System.in);
         System.out.print("Username: ");
         String username = scanner.nextLine();
         System.out.print("Password: ");
         String password = scanner.nextLine();
 
-        // Connect to DB
         Properties properties = new Properties();
         properties.setProperty("user", username);
         properties.setProperty("password", password);
