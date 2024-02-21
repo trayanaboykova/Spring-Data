c9d1fe05aeffedf57351dcedc94cd913bd7e9180
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-21 14:48:35 +0200

    Update Main.java

--- Lesson02_ORMFundamentals/Lab/ORM_Fundamentals/src/main/java/Main.java
+++ Lesson02_ORMFundamentals/Lab/ORM_Fundamentals/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import entities.Product;
 import entities.User;
 import orm.EntityManager;
 import orm.MyConnector;
@@ -8,7 +9,7 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IllegalAccessException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Username: ");
         String username = scanner.nextLine();
@@ -21,5 +22,9 @@ public class Main {
         EntityManager<User> userEntityManager = new EntityManager<>(connection);
         User taylor = new User("taylor", 13, LocalDate.now());
         userEntityManager.persist(taylor);
+
+    //    EntityManager<Product> productEntityManager = new EntityManager<>(connection);
+    //    Product song = new Product("willow", 1);
+    //    productEntityManager.persist(song);
     }
 }
