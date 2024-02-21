90a89798b8c417258b97d2f43ba64b4aa6a49a09
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-21 13:18:51 +0200

    Update Main.java

--- Lesson02_ORMFundamentals/Lab/ORM_Fundamentals/src/main/java/Main.java
+++ Lesson02_ORMFundamentals/Lab/ORM_Fundamentals/src/main/java/Main.java
@@ -1,7 +1,10 @@
+import entities.User;
+import orm.EntityManager;
 import orm.MyConnector;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +18,8 @@ public class Main {
         MyConnector.createConnection(username, password, "mini_orm");
         Connection connection = MyConnector.getConnection();
 
-
+        EntityManager<User> userEntityManager = new EntityManager<>(connection);
+        User taylor = new User("taylor", 13, LocalDate.now());
+        userEntityManager.persist(taylor);
     }
 }
