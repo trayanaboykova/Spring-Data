d839c201a29b5447c5510b6c55a82c35c2a2d184
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-21 14:48:41 +0200

    Update EntityManager.java

--- Lesson02_ORMFundamentals/Lab/ORM_Fundamentals/src/main/java/orm/EntityManager.java
+++ Lesson02_ORMFundamentals/Lab/ORM_Fundamentals/src/main/java/orm/EntityManager.java
@@ -1,6 +1,15 @@
 package orm;
 
+import orm.annotations.Column;
+import orm.annotations.Entity;
+import orm.annotations.Id;
+
+import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EntityManager<E> implements DatabaseContext<E> {
     private Connection connection;
@@ -10,8 +19,29 @@ public class EntityManager<E> implements DatabaseContext<E> {
     }
 
     @Override
-    public boolean persist(E entity) {
-        return false;
+    public boolean persist(E entity) throws SQLException, IllegalAccessException {
+        String insertStatement = "INSERT INTO %s (%s) VALUES (%s)";
+
+        // Generate INSERT
+        // Get table name
+        // Collect column without ID
+        // Collect values without ID
+        String tableName = getTableName(entity);
+
+        List<String> columnsList = getColumnsWithoutId(entity);
+        List<String> values = getColumnValuesWithoutId(entity);
+
+        String formattedInsert = String.format(insertStatement,
+                tableName,
+                String.join(",", columnsList),
+                String.join(",", values));
+
+        // Execute
+        PreparedStatement statement = connection.prepareStatement(formattedInsert);
+        int changedRows = statement.executeUpdate();
+
+        // Parse result
+        return changedRows == 1;
     }
 
     @Override
@@ -32,5 +62,54 @@ public class EntityManager<E> implements DatabaseContext<E> {
     @Override
     public E findFirst(Class<E> table, String where) {
         return null;
+    }
+
+    private String getTableName(E entity) {
+        Entity annotation = entity.getClass().getAnnotation(Entity.class);
+
+        if (annotation == null) {
+            throw new RuntimeException("No Entity annotation present");
+        }
+
+        return annotation.name();
+    }
+
+    private List<String> getColumnsWithoutId(E entity) {
+        List<String> result = new ArrayList<>();
+        Field[] declaredFields = entity.getClass().getDeclaredFields();
+
+        for (Field declaredField : declaredFields) {
+            if (declaredField.isAnnotationPresent(Id.class)) {
+                continue;
+            }
+            Column column = declaredField.getAnnotation(Column.class);
+            if (column == null) {
+                continue;
+            }
+
+            result.add(column.name());
+        }
+
+        return result;
+    }
+
+    private List<String> getColumnValuesWithoutId(E entity) throws IllegalAccessException {
+        List<String> result = new ArrayList<>();
+
+        Field[] declaredFields = entity.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            if (declaredField.isAnnotationPresent(Id.class)) {
+                continue;
+            }
+            if (!declaredField.isAnnotationPresent(Column.class)) {
+                continue;
+            }
+            declaredField.setAccessible(true);
+            Object fieldValue = declaredField.get(entity);
+
+            result.add("'" + fieldValue.toString() + "'");
+        }
+
+        return result;
     }
 }
