885f27569f1a128f0effac979789c6e9ed05436f
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-21 16:40:16 +0200

    Update User.java

--- Lesson02_ORMFundamentals/Lab/ORM_Fundamentals/src/main/java/entities/User.java
+++ Lesson02_ORMFundamentals/Lab/ORM_Fundamentals/src/main/java/entities/User.java
@@ -19,6 +19,8 @@ public class User {
     @Column(name = "registration")
     private LocalDate registration;
 
+    public User() {}
+
     public User(String username, int age, LocalDate registration) {
         this.username = username;
         this.age = age;
