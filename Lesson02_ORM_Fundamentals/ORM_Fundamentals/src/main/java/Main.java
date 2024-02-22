a0b6391507a03bebee9c012325759b83c762c7f8
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-22 11:41:06 +0200

    Update Main.java

--- Lesson02_ORM_Fundamentals/ORM_Fundamentals/src/main/java/Main.java
+++ Lesson02_ORM_Fundamentals/ORM_Fundamentals/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import entities.Order;
+import entities.Product;
 import entities.User;
 import orm.EntityManager;
 import orm.MyConnector;
@@ -20,20 +21,29 @@ public class Main {
         MyConnector.createConnection(username, password, "mini_orm");
         Connection connection = MyConnector.getConnection();
 
-        EntityManager<User> userEntityManager = new EntityManager<>(connection);
-        User taylor = new User("taylor", 33, LocalDate.now());
-        taylor.setId(1);
-        userEntityManager.persist(taylor);
+        // EntityManager<User> userEntityManager = new EntityManager<>(connection);
+        // userEntityManager.doCreate(User.class);
+        // EntityManager<User> userEntityManager = new EntityManager<>(connection);
+        // User taylor = new User("taylor", 33, LocalDate.now());
+        // userEntityManager.persist(taylor);
+        // User taylor = new User("taylor", 33, LocalDate.now());
+        // taylor.setId(1);
+        // userEntityManager.persist(taylor);
+        // Iterable<User> users = userEntityManager.find(User.class);
+        // System.out.println(users.iterator().next());
 
-        Iterable<User> users = userEntityManager.find(User.class);
-        System.out.println(users.iterator().next());
+
+        //  EntityManager<Order> orderEntityManager = new EntityManager<>(connection);
+        //  orderEntityManager.doCreate(Order.class);
+        // EntityManager<Order> orderEntityManager = new EntityManager<>(connection);
+        // Order tay13 = new Order("tay13", LocalDate.now());
+        // orderEntityManager.persist(tay13);
+
 
         //    EntityManager<Product> productEntityManager = new EntityManager<>(connection);
         //    Product song = new Product("willow", 1);
         //    productEntityManager.persist(song);
 
-        EntityManager<Order> orderEntityManager = new EntityManager<>(connection);
-        Order tay13 = new Order("tay13", LocalDate.now());
-        orderEntityManager.persist(tay13);
+
     }
 }
