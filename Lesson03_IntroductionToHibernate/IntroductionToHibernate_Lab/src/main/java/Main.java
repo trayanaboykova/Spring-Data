efa4f63e3b7efda7ba0cce5d9d142b7616cb744d
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-24 15:59:24 +0200

    Update Main.java

--- Lesson03_IntroductionToHibernate/IntroductionToHibernate_Lab/src/main/java/Main.java
+++ Lesson03_IntroductionToHibernate/IntroductionToHibernate_Lab/src/main/java/Main.java
@@ -1,5 +1,9 @@
+import entities.Student;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +11,26 @@ public class Main {
         configuration.configure();
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        sessionFactory.openSession();
+        Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+
+        //    Student student = new Student();
+        //    student.setName("Taylor");
+        //    session.save(student);
+
+        //     Student student = session.get(Student.class, 1);
+        //     System.out.printf("%d - %s", student.getId(), student.getName());
+        //     student.setName("Updated");
+
+        List<Student> students = session
+                .createQuery("FROM Student", Student.class).list();
+
+        for (Student current : students) {
+            System.out.println(current);
+        }
+
+        session.getTransaction().commit();
+        session.close();
     }
 }
