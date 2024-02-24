3141e072b3b9b92e8a749cdfe03d3eee34b8d89b
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-24 15:59:21 +0200

    Update Student.java

--- Lesson03_IntroductionToHibernate/IntroductionToHibernate_Lab/src/main/java/entities/Student.java
+++ Lesson03_IntroductionToHibernate/IntroductionToHibernate_Lab/src/main/java/entities/Student.java
@@ -19,4 +19,12 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
