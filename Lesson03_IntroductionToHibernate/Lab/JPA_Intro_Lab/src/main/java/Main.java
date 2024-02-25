8235c570e8eae14007495694cc7a66f9cc40009c
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-25 12:26:55 +0200

    Update Main.java

--- Lesson03_IntroductionToHibernate/Lab/JPA_Intro_Lab/src/main/java/Main.java
+++ Lesson03_IntroductionToHibernate/Lab/JPA_Intro_Lab/src/main/java/Main.java
@@ -1,5 +1,24 @@
+import entities.Teacher;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory emf =
+           Persistence.createEntityManagerFactory("general");
 
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+    //    Teacher teacher = new Teacher();
+    //    teacher.setName("Taylor");
+    //    entityManager.persist(teacher);
+
+        Teacher fromDB = entityManager.find(Teacher.class, 3);
+        System.out.println(fromDB);
+
+        entityManager.getTransaction().commit();
     }
 }
