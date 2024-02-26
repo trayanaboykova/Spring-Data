ae36879ef91ccc04eb0bef8190b7fa5c09584b92
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-26 10:38:38 +0200

    deleting files

--- Lesson03_IntroductionToHibernate/Exercise/IntroductionToHibernate_Exercise/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-public class Main {
-    public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("softuni_jpa");
-    }
-}
