a061582c6a1003b2a91d56acd785f8c93af697e7
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-26 10:53:34 +0200

    Create Main.java

---
+++ Lesson03_IntroductionToHibernate/Exercise/IntroductionToHibernate_Exercise/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("softuni_jpa");
+    }
+}
