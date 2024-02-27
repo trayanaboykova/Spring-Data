8c9d08fb7e5148dcf06d1fd29c912c39d2d63b85
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-27 19:41:03 +0200

    Update Main.java

--- Lesson04_HibernateCodeFirst/HibernateCodeFirst_Lab/src/main/java/lab/Main.java
+++ Lesson04_HibernateCodeFirst/HibernateCodeFirst_Lab/src/main/java/lab/Main.java
@@ -3,10 +3,12 @@ package lab;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import lab.composition.Company;
 import lab.composition.PlateNumber;
 import lab.inheritance.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,22 +18,41 @@ public class Main {
 
         entityManager.getTransaction().begin();
 
-        PlateNumber plate = new PlateNumber("3030");
+        // persistAll(entityManager);
 
-        Vehicle car = new Car("BMW X1", BigDecimal.TEN, "Petrol", 5, plate);
-        Vehicle bike = new Bike("BMX", BigDecimal.ONE, "None");
-        Vehicle plane = new Plane("Boeing", BigDecimal.TEN, "PlaneFuel", 100);
-        Vehicle truck = new Truck("John Deere", BigDecimal.TEN, "Diesel", 40);
-
-        entityManager.persist(car);
-        entityManager.persist(bike);
-        entityManager.persist(plane);
-        entityManager.persist(truck);
-        entityManager.persist(plate);
+        // find(entityManager);
 
         entityManager.getTransaction().commit();
 
         entityManager.close();
 
+    }
+
+    private static void find(EntityManager entityManager) {
+        Company company = entityManager.find(Company.class, 2);
+
+        List<Plane> planes = company.getPlanes();
+
+        for (Plane plane : planes) {
+            System.out.println(plane);
+        }
+    }
+
+    private static void persistAll(EntityManager entityManager) {
+        PlateNumber plate = new PlateNumber("3030");
+
+        Company company = new Company("Bulgaria Air");
+
+        Vehicle car = new Car("BMW X1", BigDecimal.TEN, "Petrol", 5, plate);
+        Vehicle bike = new Bike("BMX", BigDecimal.ONE, "None");
+        Plane plane = new Plane("Boeing", BigDecimal.TEN, "PlaneFuel", 100, company);
+        Vehicle truck = new Truck("John Deere", BigDecimal.TEN, "Diesel", 40);
+
+        entityManager.persist(company);
+        entityManager.persist(car);
+        entityManager.persist(bike);
+        entityManager.persist(plane);
+        entityManager.persist(truck);
+        entityManager.persist(plate);
     }
 }
