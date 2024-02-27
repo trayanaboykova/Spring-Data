465e233c4f6599aad5288bdb6240534f6c932371
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-27 18:51:25 +0200

    Update Plane.java

--- Lesson04_HibernateCodeFirst/HibernateCodeFirst_Lab/src/main/java/lab/inheritance/Plane.java
+++ Lesson04_HibernateCodeFirst/HibernateCodeFirst_Lab/src/main/java/lab/inheritance/Plane.java
@@ -2,6 +2,8 @@ package lab.inheritance;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lab.composition.Company;
 
 import java.math.BigDecimal;
 
@@ -10,6 +12,8 @@ public class Plane extends Vehicle {
     private static final String PLANE_TYPE = "PLANE";
     @Column(name = "passenger_capacity")
     private int passengerCapacity;
+    @ManyToOne
+    private Company owner;
 
     public Plane() {}
     public Plane(String model, BigDecimal price, String fuelType, int passengerCapacity) {
