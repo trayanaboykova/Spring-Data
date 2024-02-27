f660c3f62702450a62f34b00bb49eafbab2be00b
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-27 19:03:12 +0200

    Update Truck.java

--- Lesson04_HibernateCodeFirst/HibernateCodeFirst_Lab/src/main/java/lab/inheritance/Truck.java
+++ Lesson04_HibernateCodeFirst/HibernateCodeFirst_Lab/src/main/java/lab/inheritance/Truck.java
@@ -1,15 +1,23 @@
 package lab.inheritance;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lab.composition.Driver;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 public class Truck extends Vehicle {
     private static final String TRUCK_TYPE = "TRUCK";
     @Column(name = "load_capacity")
     private double loadCapacity;
+
+    @ManyToMany
+    @JoinTable(name = "trucks_drivers",
+            joinColumns = @JoinColumn(name = "truck_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "driver_id", referencedColumnName = "id")
+    )
+    private List<Driver> drivers;
 
     public Truck() {}
 
