4a8aff7cc88f01ba0da1aeaecdb8500d84a7a7af
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-27 15:19:52 +0200

    Update Main.java

--- Lesson04_HibernateCodeFirst/Lab/HibernateCodeFirst_Lab/src/main/java/inheritance/Main.java
+++ Lesson04_HibernateCodeFirst/Lab/HibernateCodeFirst_Lab/src/main/java/inheritance/Main.java
@@ -1,7 +1,13 @@
 package inheritance;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-
+        EntityManagerFactory mainFactory =
+                Persistence.createEntityManagerFactory("main");
+        EntityManager entityManager = mainFactory.createEntityManager();
     }
 }
