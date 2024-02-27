9054ec29de15c97a936f1d2bac9f389c31a8d62b
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-27 18:07:52 +0200

    Update Car.java

--- Lesson04_HibernateCodeFirst/HibernateCodeFirst_Lab/src/main/java/lab/inheritance/Car.java
+++ Lesson04_HibernateCodeFirst/HibernateCodeFirst_Lab/src/main/java/lab/inheritance/Car.java
@@ -2,6 +2,8 @@ package lab.inheritance;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lab.composition.PlateNumber;
 
 import java.math.BigDecimal;
 
@@ -10,11 +12,15 @@ public class Car extends Vehicle {
     private static final String CAR_TYPE = "CAR";
     @Basic
     private int seats;
+    @OneToOne
+    private PlateNumber plateNumber;
 
     public Car() {}
 
-    public Car(String model, BigDecimal price, String fuelType, int seats) {
+    public Car(String model, BigDecimal price, String fuelType, int seats, PlateNumber plateNumber) {
         super(CAR_TYPE, model, price, fuelType);
+
         this.seats = seats;
+        this.plateNumber = plateNumber;
     }
 }
