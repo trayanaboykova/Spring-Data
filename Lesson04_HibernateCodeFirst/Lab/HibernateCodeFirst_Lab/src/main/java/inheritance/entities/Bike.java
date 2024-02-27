b96af33bc4bbe55574d81257d88202418caab6cc
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-27 16:11:41 +0200

    Update Bike.java

--- Lesson04_HibernateCodeFirst/Lab/HibernateCodeFirst_Lab/src/main/java/inheritance/entities/Bike.java
+++ Lesson04_HibernateCodeFirst/Lab/HibernateCodeFirst_Lab/src/main/java/inheritance/entities/Bike.java
@@ -1,10 +1,13 @@
 package inheritance.entities;
 
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "bikes")
+@DiscriminatorValue("OurBike")
 public class Bike extends Vehicle {
     private static final String BIKE_TYPE = "BIKE";
     public Bike() {
