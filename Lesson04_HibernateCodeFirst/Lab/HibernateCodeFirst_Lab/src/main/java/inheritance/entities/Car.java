e88f4dd38b5d3249a72941a216e93cadf0b9f0a3
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-27 16:11:44 +0200

    Update Car.java

--- Lesson04_HibernateCodeFirst/Lab/HibernateCodeFirst_Lab/src/main/java/inheritance/entities/Car.java
+++ Lesson04_HibernateCodeFirst/Lab/HibernateCodeFirst_Lab/src/main/java/inheritance/entities/Car.java
@@ -1,10 +1,12 @@
 package inheritance.entities;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "cars")
+@DiscriminatorValue("OurCar")
 public class Car extends Vehicle {
     private static final String CAR_TYPE = "CAR";
     public Car() {
