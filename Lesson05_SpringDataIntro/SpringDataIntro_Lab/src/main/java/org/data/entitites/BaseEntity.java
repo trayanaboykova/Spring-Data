08ad6b26773901a63c93a172e422bb53261feb69
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-03 18:04:58 +0200

    Create BaseEntity.java

---
+++ Lesson05_SpringDataIntro/SpringDataIntro_Lab/src/main/java/org/data/entitites/BaseEntity.java
@@ -0,0 +1,19 @@
+package org.data.entitites;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    protected BaseEntity(){}
+
+    public int getId() {
+        return id;
+    }
+}
