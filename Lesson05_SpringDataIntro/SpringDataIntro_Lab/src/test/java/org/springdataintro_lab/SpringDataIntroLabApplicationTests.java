e90eac41c15eee708c3344f477f98595f97ad17d
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-03 12:54:20 +0200

    Create SpringDataIntroLabApplicationTests.java

---
+++ Lesson05_SpringDataIntro/SpringDataIntro_Lab/src/test/java/org/springdataintro_lab/SpringDataIntroLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springdataintro_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataIntroLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
