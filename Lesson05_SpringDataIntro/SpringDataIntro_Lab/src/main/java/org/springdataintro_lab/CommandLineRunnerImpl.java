f0cb79f4f8d59d068d1a60bde3ad2f45dcf8bf8d
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-04 12:59:34 +0200

    Update CommandLineRunnerImpl.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Lab/src/main/java/org/springdataintro_lab/CommandLineRunnerImpl.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Lab/src/main/java/org/springdataintro_lab/CommandLineRunnerImpl.java
@@ -2,6 +2,7 @@ package org.springdataintro_lab;
 
 import org.springdataintro_lab.data.entitites.User;
 import org.springdataintro_lab.data.repositories.UserRepository;
+import org.springdataintro_lab.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,16 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
     @Autowired
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public CommandLineRunnerImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public CommandLineRunnerImpl(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         User user = new User("Taylor", 30);
-        this.userRepository.saveAndFlush(user);
+        this.userService.register(user);
         System.out.println();
     }
 }
