4f9b5de4f25d9b681e356b622c2f3dfad721c28e
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 15:33:41 +0200

    Update BookService.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/service/BookService.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/service/BookService.java
@@ -1,7 +1,13 @@
 package org.springdataintro_exercise.service;
 
+import org.springdataintro_exercise.data.entities.Book;
+
 import java.io.IOException;
+import java.util.List;
+
 
 public interface BookService {
     void seedBooks() throws IOException;
+
+    List<String> findAllBooksAfterYear2000();
 }
