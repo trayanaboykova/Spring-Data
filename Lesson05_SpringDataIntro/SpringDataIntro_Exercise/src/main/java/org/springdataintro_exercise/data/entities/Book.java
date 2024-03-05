e6a0239e0e9b9363b5a07d7c9942a3280427bf96
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 11:23:00 +0200

    Create Book.java

---
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/data/entities/Book.java
@@ -0,0 +1,86 @@
+package org.springdataintro_exercise.data.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "books")
+public class Book extends BaseEntity {
+    @Column(length = 50, nullable = false)
+    private String title;
+    @Column(length = 1000, nullable = false)
+    private String description;
+    @Enumerated(value = EnumType.STRING)
+    private EditionType editionType;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private int copies;
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+    @Enumerated(value = EnumType.STRING)
+    private AgeRestriction ageRestriction;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author author;
+    @ManyToMany
+    @JoinTable(name = "books_categories",
+    joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id", referencedColumnName = "id"))
+    private Set<Category> categories;
+
+    public Book(String title, EditionType editionType, BigDecimal price, int copies, LocalDate releaseDate, AgeRestriction ageRestriction, Author author, Set<Category> categories) {
+        this.title = title;
+        this.editionType = editionType;
+        this.price = price;
+        this.copies = copies;
+        this.releaseDate = releaseDate;
+        this.ageRestriction = ageRestriction;
+        this.author = author;
+        this.categories = categories;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public EditionType getEditionType() {
+        return editionType;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public AgeRestriction getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+}
