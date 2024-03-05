5131b3d5aba31d28b1acf19efb4e10539ff5d52c
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 10:04:14 +0200

    Update UserService.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Lab/src/main/java/org/springdataintro_lab/service/UserService.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Lab/src/main/java/org/springdataintro_lab/service/UserService.java
@@ -4,4 +4,8 @@ import org.springdataintro_lab.data.entitites.User;
 
 public interface UserService {
     void register(User user);
+
+    User findUserById(int id);
+
+    User findByName(String name);
 }
