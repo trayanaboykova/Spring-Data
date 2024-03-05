ccc3c99f3d7b932b5625f0bf60954e8b2ae520da
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 15:33:39 +0200

    Update BookRepository.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/data/repositories/BookRepository.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/data/repositories/BookRepository.java
@@ -4,6 +4,12 @@ import org.springdataintro_exercise.data.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    // FROM Book b WHERE b.releaseDate > ?1
+    Set<Book> findAllByReleaseDateAfter(LocalDate date);
 }
