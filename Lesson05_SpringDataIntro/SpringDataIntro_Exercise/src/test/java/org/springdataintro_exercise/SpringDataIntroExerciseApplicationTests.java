515e19b7ea095a7b3e8a5d8611b0439a785b48b9
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 10:42:39 +0200

    adding files

---
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/test/java/org/springdataintro_exercise/SpringDataIntroExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springdataintro_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataIntroExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
