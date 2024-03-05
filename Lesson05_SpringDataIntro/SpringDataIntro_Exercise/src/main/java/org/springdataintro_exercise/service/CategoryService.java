cea52babfd429afa601d2817100a33723ffbb08a
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 14:05:10 +0200

    Update CategoryService.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/service/CategoryService.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/service/CategoryService.java
@@ -1,7 +1,12 @@
 package org.springdataintro_exercise.service;
 
+import org.springdataintro_exercise.data.entities.Category;
+
 import java.io.IOException;
+import java.util.Set;
 
 public interface CategoryService {
     void seedCategories() throws IOException;
+
+    Set<Category> getRandomCategories();
 }
