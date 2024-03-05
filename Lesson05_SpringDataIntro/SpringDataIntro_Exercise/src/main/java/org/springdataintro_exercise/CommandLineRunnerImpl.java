12f215690f6cec261c06ccbf92059e5bfa56dca6
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 14:22:26 +0200

    Update CommandLineRunnerImpl.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/CommandLineRunnerImpl.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/CommandLineRunnerImpl.java
@@ -1,10 +1,31 @@
 package org.springdataintro_exercise;
 
+import org.springdataintro_exercise.service.AuthorService;
+import org.springdataintro_exercise.service.BookService;
+import org.springdataintro_exercise.service.CategoryService;
 import org.springframework.boot.CommandLineRunner;
 
+import java.io.IOException;
+
 public class CommandLineRunnerImpl implements CommandLineRunner {
+    private final AuthorService authorService;
+    private final CategoryService categoryService;
+    private final BookService bookService;
+
+    public CommandLineRunnerImpl(AuthorService authorService, CategoryService categoryService, BookService bookService) {
+        this.authorService = authorService;
+        this.categoryService = categoryService;
+        this.bookService = bookService;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Started");
+        seedData();
+    }
+
+    private void seedData() throws IOException {
+        this.categoryService.seedCategories();
+        this.authorService.seedAuthors();
+        this.bookService.seedBooks();
     }
 }
