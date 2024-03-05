54913b58706ec7931e7fab58a80bb1531965e3b7
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 11:22:50 +0200

    Create CommandLineRunnerImpl.java

---
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/CommandLineRunnerImpl.java
@@ -0,0 +1,10 @@
+package org.springdataintro_exercise;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class CommandLineRunnerImpl implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Started");
+    }
+}
