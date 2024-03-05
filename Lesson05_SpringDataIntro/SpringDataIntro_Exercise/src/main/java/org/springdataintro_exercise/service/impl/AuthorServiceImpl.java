a882d760063e3182ebf444b555d8d41637a1b375
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 13:35:51 +0200

    Create AuthorServiceImpl.java

---
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/service/impl/AuthorServiceImpl.java
@@ -0,0 +1,31 @@
+package org.springdataintro_exercise.service.impl;
+
+import org.springdataintro_exercise.data.entities.Author;
+import org.springdataintro_exercise.data.repositories.AuthorRepository;
+import org.springdataintro_exercise.service.AuthorService;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+    private final AuthorRepository authorRepository;
+    private static final String FILE_PATH = "src/main/resources/files/authors.txt";
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public void seedAuthors() throws IOException {
+        Files.readAllLines(Path.of(FILE_PATH))
+                .stream()
+                .filter(row -> !row.isEmpty())
+                .forEach(row -> {
+                    String[] tokens = row.split("\\s+");
+                    this.authorRepository.saveAndFlush(new Author(tokens[0], tokens[1]));
+                });
+    }
+}
