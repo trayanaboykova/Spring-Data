5ecef6595e3fd41842966513bcc4d6fd975dbdd7
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 11:22:55 +0200

    Create Author.java

---
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/data/entities/Author.java
@@ -0,0 +1,31 @@
+package org.springdataintro_exercise.data.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "authors")
+public class Author extends BaseEntity {
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+}
