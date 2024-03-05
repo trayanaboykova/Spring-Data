4a452c27b4949576b368f9336f2971bb5f5ef5cc
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 16:01:55 +0200

    Update AuthorRepository.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/data/repositories/AuthorRepository.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/data/repositories/AuthorRepository.java
@@ -4,6 +4,12 @@ import org.springdataintro_exercise.data.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    Set<Author> findAllByBooksReleaseDateBefore(LocalDate releaseDate);
+    
 }
