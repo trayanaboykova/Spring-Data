5ca2d58f63f24f9602c6843da48c610bb75263fe
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 14:22:29 +0200

    Update Category.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/data/entities/Category.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/data/entities/Category.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import org.springdataintro_exercise.data.entities.base.BaseEntity;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "categories")
 public class Category extends BaseEntity {
@@ -20,5 +22,18 @@ public class Category extends BaseEntity {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
