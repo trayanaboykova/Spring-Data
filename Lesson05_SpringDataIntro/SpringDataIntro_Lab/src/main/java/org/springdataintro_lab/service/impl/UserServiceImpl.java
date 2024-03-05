5049047d0b6c3b6f10570f0638b7a9d4f8d3491a
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 10:04:12 +0200

    Update UserServiceImpl.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Lab/src/main/java/org/springdataintro_lab/service/impl/UserServiceImpl.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Lab/src/main/java/org/springdataintro_lab/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import org.springdataintro_lab.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -20,4 +22,21 @@ public class UserServiceImpl implements UserService {
     public void register(User user) {
         this.userRepository.saveAndFlush(user);
     }
+
+    @Override
+    public User findUserById(int id) {
+        Optional<User> optional = this.userRepository.findById(id);
+        if (optional.isPresent()) {
+           return optional.get();
+        }
+        System.out.println("No such user with given id - " + id);
+        return null;
+    }
+
+    @Override
+    public User findByName(String name) {
+        return this.userRepository.findByUsername(name);
+    }
+
+
 }
