482b664b53e63475e3f3dc353a832242ff4c9fa8
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 13:39:49 +0200

    Create BookServiceImpl.java

---
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/service/impl/BookServiceImpl.java
@@ -0,0 +1,8 @@
+package org.springdataintro_exercise.service.impl;
+
+import org.springdataintro_exercise.service.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+}
