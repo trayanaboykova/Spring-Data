84cc5cde524c787699b7341f8c88edd42899cc1e
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-05 14:00:26 +0200

    Update AuthorService.java

--- Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/service/AuthorService.java
+++ Lesson05_SpringDataIntro/SpringDataIntro_Exercise/src/main/java/org/springdataintro_exercise/service/AuthorService.java
@@ -1,7 +1,12 @@
 package org.springdataintro_exercise.service;
 
+import org.springdataintro_exercise.data.entities.Author;
+
 import java.io.IOException;
 
 public interface AuthorService {
     void seedAuthors() throws IOException;
+
+    Author getRandomAuthor();
+
 }
