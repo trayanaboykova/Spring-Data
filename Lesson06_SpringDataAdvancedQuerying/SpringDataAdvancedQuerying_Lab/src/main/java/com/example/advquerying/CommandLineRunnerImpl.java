fdf0d1aba96394ff88ff7c87f6e6f8476cc3d865
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-06 13:42:10 +0200

    Update CommandLineRunnerImpl.java

--- Lesson06_SpringDataAdvancedQuerying/SpringDataAdvancedQuerying_Lab/src/main/java/com/example/advquerying/CommandLineRunnerImpl.java
+++ Lesson06_SpringDataAdvancedQuerying/SpringDataAdvancedQuerying_Lab/src/main/java/com/example/advquerying/CommandLineRunnerImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
@@ -19,7 +21,13 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        this.shampooService.getAllShampoosByGivenSize(reader.readLine())
+        //   this.shampooService.getAllShampoosByGivenSize(reader.readLine())
+        //           .forEach(System.out::println);
+        // this.shampooService.getAllShampoosContainingIngredient(List.of(reader.readLine().split(" ")))
+        //         .forEach(System.out::println);
+        // this.shampooService.getAllShampoosByGivenSizeOrLabel("medium", 10)
+        //         .forEach(System.out::println);
+        this.shampooService.getAllShampoosWithPriceHigherThan(BigDecimal.valueOf(5))
                 .forEach(System.out::println);
     }
 }
