ef71e8b9ef50e8633e38220f9e7116b950686c12
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-06 13:42:17 +0200

    Update ShampooServiceImpl.java

--- Lesson06_SpringDataAdvancedQuerying/SpringDataAdvancedQuerying_Lab/src/main/java/com/example/advquerying/service/impl/ShampooServiceImpl.java
+++ Lesson06_SpringDataAdvancedQuerying/SpringDataAdvancedQuerying_Lab/src/main/java/com/example/advquerying/service/impl/ShampooServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.advquerying.service.impl;
 
+import com.example.advquerying.entities.Shampoo;
 import com.example.advquerying.entities.Size;
 import com.example.advquerying.repositories.ShampooRepository;
 import com.example.advquerying.service.ShampooService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,12 +22,48 @@ public class ShampooServiceImpl implements ShampooService {
     @Override
     public List<String> getAllShampoosByGivenSize(String size) {
         Size sizeEnum = Size.valueOf(size.toUpperCase());
-        return this.shampooRepository.findAllBySizeOrderById(sizeEnum)
+        Set<Shampoo> allBySizeOrderById = this.shampooRepository.findAllBySizeOrderById(sizeEnum);
+        return allBySizeOrderById
                 .stream()
                 .map(s -> String.format("%s %s %.2flv.",
                         s.getBrand(),
                         s.getSize().name(),
                         s.getPrice().doubleValue()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> getAllShampoosByGivenSizeOrLabel(String size, long id) {
+        Size sizeEnum = Size.valueOf(size.toUpperCase());
+        Set<Shampoo> allBySizeOrderById = this.shampooRepository.findAllBySizeOrLabelIdOrderByPrice(sizeEnum, id);
+        return allBySizeOrderById
+                .stream()
+                .map(s -> String.format("%s %s %.2flv.",
+                        s.getBrand(),
+                        s.getSize().name(),
+                        s.getPrice().doubleValue()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> getAllShampoosWithPriceHigherThan(BigDecimal price) {
+        Set<Shampoo> allBySizeOrderById = this.shampooRepository.findAllByPriceGreaterThanOrderByPriceDesc(price);
+        return allBySizeOrderById
+                .stream()
+                .map(s -> String.format("%s %s %.2flv.",
+                        s.getBrand(),
+                        s.getSize().name(),
+                        s.getPrice().doubleValue()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> getAllShampoosContainingIngredient(List<String> strings) {
+        Set<Shampoo> allByIngredientsNameIn = this.shampooRepository.findAllByIngredientsNameIn(strings);
+
+        return allByIngredientsNameIn
+                .stream()
+                .map(Shampoo::getBrand)
                 .collect(Collectors.toList());
     }
 }
