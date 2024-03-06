673c72a48ffd664d6a10cb43da43bca4db30d905
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-06 13:42:15 +0200

    Update ShampooRepository.java

--- Lesson06_SpringDataAdvancedQuerying/SpringDataAdvancedQuerying_Lab/src/main/java/com/example/advquerying/repositories/ShampooRepository.java
+++ Lesson06_SpringDataAdvancedQuerying/SpringDataAdvancedQuerying_Lab/src/main/java/com/example/advquerying/repositories/ShampooRepository.java
@@ -5,9 +5,15 @@ import com.example.advquerying.entities.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 
 @Repository
 public interface ShampooRepository extends JpaRepository<Shampoo, Long> {
     Set<Shampoo> findAllBySizeOrderById(Size size);
+    Set<Shampoo> findAllBySizeOrLabelIdOrderByPrice(Size size, long id);
+    Set<Shampoo> findAllByPriceGreaterThanOrderByPriceDesc(BigDecimal price);
+
+    Set<Shampoo> findAllByIngredientsNameIn(List<String> names);
 }
