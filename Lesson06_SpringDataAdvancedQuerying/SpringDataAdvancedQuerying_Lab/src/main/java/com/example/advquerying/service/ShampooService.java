76b0e7a4014bea4f807b76a76d89a5863c95e022
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-06 13:42:20 +0200

    Update ShampooService.java

--- Lesson06_SpringDataAdvancedQuerying/SpringDataAdvancedQuerying_Lab/src/main/java/com/example/advquerying/service/ShampooService.java
+++ Lesson06_SpringDataAdvancedQuerying/SpringDataAdvancedQuerying_Lab/src/main/java/com/example/advquerying/service/ShampooService.java
@@ -1,7 +1,14 @@
 package com.example.advquerying.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ShampooService {
     List<String> getAllShampoosByGivenSize(String size);
+
+    List<String> getAllShampoosByGivenSizeOrLabel(String size, long id);
+
+    List<String> getAllShampoosWithPriceHigherThan(BigDecimal price);
+
+    List<String> getAllShampoosContainingIngredient(List<String> strings);
 }
