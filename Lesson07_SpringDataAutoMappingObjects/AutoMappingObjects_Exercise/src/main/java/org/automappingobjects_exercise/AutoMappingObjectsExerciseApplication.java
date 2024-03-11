f02a0d6d40042eef835b13c7a8fc13d85a85e506
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-11 10:58:13 +0200

    adding exercise

---
+++ Lesson07_SpringDataAutoMappingObjects/AutoMappingObjects_Exercise/src/main/java/org/automappingobjects_exercise/AutoMappingObjectsExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.automappingobjects_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMappingObjectsExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMappingObjectsExerciseApplication.class, args);
+	}
+
+}
---
+++ Lesson07_SpringDataAutoMappingObjects/AutoMappingObjects_Exercise/src/test/java/org/automappingobjects_exercise/AutoMappingObjectsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.automappingobjects_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMappingObjectsExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
