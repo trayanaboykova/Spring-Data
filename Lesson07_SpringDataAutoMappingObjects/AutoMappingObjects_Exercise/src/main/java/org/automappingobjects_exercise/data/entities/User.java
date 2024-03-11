c6064f701c2d7fb8bfcd76721776fee53610c476
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-11 11:11:27 +0200

    Create User.java

---
+++ Lesson07_SpringDataAutoMappingObjects/AutoMappingObjects_Exercise/src/main/java/org/automappingobjects_exercise/data/entities/User.java
@@ -0,0 +1,53 @@
+package org.automappingobjects_exercise.data.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "is_admin")
+    private boolean isAdmin;
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
