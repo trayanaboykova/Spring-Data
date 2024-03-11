2d4139350fa44b95d99ec607b3f2a459353dfed7
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-11 11:11:18 +0200

    Create CommandLineRunnerImpl.java

---
+++ Lesson07_SpringDataAutoMappingObjects/AutoMappingObjects_Exercise/src/main/java/org/automappingobjects_exercise/CommandLineRunnerImpl.java
@@ -0,0 +1,12 @@
+package org.automappingobjects_exercise;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
