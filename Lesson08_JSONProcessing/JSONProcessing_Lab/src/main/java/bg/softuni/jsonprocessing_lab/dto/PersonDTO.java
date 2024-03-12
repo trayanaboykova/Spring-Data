f98391847cd8c4d148d95a99f7d36c8a0c5f2ea1
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-12 18:47:50 +0200

    Update PersonDTO.java

--- Lesson08_JSONProcessing/JSONProcessing_Lab/src/main/java/bg/softuni/jsonprocessing_lab/dto/PersonDTO.java
+++ Lesson08_JSONProcessing/JSONProcessing_Lab/src/main/java/bg/softuni/jsonprocessing_lab/dto/PersonDTO.java
@@ -1,15 +1,33 @@
 package bg.softuni.jsonprocessing_lab.dto;
 
+import java.util.List;
+
 public class PersonDTO {
     private String firstName;
     private String lastName;
     private int age;
     private boolean isMarried;
+    private List<Integer> lotteryNumbers;
+    private AddressDTO address;
 
-    public PersonDTO(String firstName, String lastName, int age, boolean isMarried) {
+    public PersonDTO(String firstName, String lastName, int age, boolean isMarried, List<Integer> lotteryNumbers, AddressDTO address) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.isMarried = isMarried;
+        this.lotteryNumbers = lotteryNumbers;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDTO{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", isMarried=" + isMarried +
+                ", lotteryNumbers=" + lotteryNumbers +
+                ", address=" + address +
+                '}';
     }
 }
