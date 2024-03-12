18b3d7a3861a6d94b286d4650e959d806aa0f81d
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-12 17:52:36 +0200

    adding files

---
+++ Lesson08_JSONProcessing/JSONProcessing_Lab/src/test/java/org/jsonprocessing_lab/JsonProcessingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsonprocessing_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonProcessingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
