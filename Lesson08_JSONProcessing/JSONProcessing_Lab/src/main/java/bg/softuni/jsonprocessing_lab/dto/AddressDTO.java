9778295953f86dbe2a0fa4626c7a279224201459
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-12 18:47:47 +0200

    Create AddressDTO.java

---
+++ Lesson08_JSONProcessing/JSONProcessing_Lab/src/main/java/bg/softuni/jsonprocessing_lab/dto/AddressDTO.java
@@ -0,0 +1,11 @@
+package bg.softuni.jsonprocessing_lab.dto;
+
+public class AddressDTO {
+    private String country;
+    private String city;
+
+    public AddressDTO(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+}
