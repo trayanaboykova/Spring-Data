29b3609e52bf037e415adbca53177186f228e902
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-12 22:50:21 +0200

    Create Config.java

---
+++ Lesson08_JSONProcessing/JSONProcessing_Lab/src/main/java/bg/softuni/jsonprocessing_lab/config/Config.java
@@ -0,0 +1,43 @@
+package bg.softuni.jsonprocessing_lab.config;
+
+import bg.softuni.jsonprocessing_lab.services.AddressService;
+import bg.softuni.jsonprocessing_lab.services.PersonService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class Config {
+    @Bean
+    public Gson createGSON() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    @Bean("withNulls")
+    @Primary
+    public Gson createGSONWithNulls() {
+        return new GsonBuilder()
+                .serializeNulls()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    @Bean("withoutNulls")
+    public Gson createGSONWithoutNulls() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+    }
+    @Bean
+    public PersonService personService(
+            AddressService addressService,
+            @Value("${yourproject.yourkey.config1}") String config1) {
+        System.out.println(config1);
+        return new PersonService(addressService);
+    }
+}
