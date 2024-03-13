c4224c99e4ceb0c6cfc27e43d223d04d1d04fad7
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-13 17:17:59 +0200

    Update UserSeedDto.java

--- Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/service/dtos/UserSeedDto.java
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/service/dtos/UserSeedDto.java
@@ -1,6 +1,43 @@
 package org.jsonprocessing_exercise.service.dtos;
 
+import com.google.gson.annotations.Expose;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.io.Serializable;
 
 public class UserSeedDto implements Serializable {
+    @Expose
+    @NotNull
+    private String firstName;
+    @Expose
+    @NotNull
+    private String lastName;
+    @Expose
+    @Min(18)
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
