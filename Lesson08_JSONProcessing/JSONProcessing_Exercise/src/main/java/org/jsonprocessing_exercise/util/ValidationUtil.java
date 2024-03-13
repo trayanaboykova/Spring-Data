54b761ae05e0e1c3893371e5345bbf57a4a42793
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-13 13:26:28 +0200

    adding validation class and interface

---
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/util/ValidationUtil.java
@@ -0,0 +1,10 @@
+package org.jsonprocessing_exercise.util;
+
+import jakarta.validation.ConstraintViolation;
+
+import java.util.Set;
+
+public interface ValidationUtil {
+    <E> boolean isValid(E entity);
+    <E> Set<ConstraintViolation<E>> getViolations(E entity);
+}
