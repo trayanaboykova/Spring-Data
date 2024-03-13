ca107f0290b002febab7b57836d165d97e131dfe
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-13 07:31:33 +0200

    Create HomeController.java

---
+++ Lesson08_JSONProcessing/JSONProcessing_Lab/src/main/java/bg/softuni/jsonprocessing_lab/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package bg.softuni.jsonprocessing_lab.controllers;
+
+
+import bg.softuni.jsonprocessing_lab.dto.PersonDTO;
+import jakarta.validation.Valid;
+
+public class HomeController {
+
+//    public PersonDTO getPerson(int id) {
+//        return .....;
+//    }
+
+    public void updatePerson(int id, @Valid PersonDTO data) {
+    }
+}
