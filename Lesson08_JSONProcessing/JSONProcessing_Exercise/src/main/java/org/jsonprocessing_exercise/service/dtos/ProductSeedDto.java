2c442c487a91dd0f93b6457a07167543361813ed
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-13 18:35:29 +0200

    Update ProductSeedDto.java

--- Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/service/dtos/ProductSeedDto.java
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/service/dtos/ProductSeedDto.java
@@ -1,7 +1,34 @@
 package org.jsonprocessing_exercise.service.dtos;
 
+import com.google.gson.annotations.Expose;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 public class ProductSeedDto implements Serializable {
+    @Expose
+    @NotNull
+    @Size(min = 3)
+    private String name;
+    @Expose
+    @NotNull
+    private BigDecimal price;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
