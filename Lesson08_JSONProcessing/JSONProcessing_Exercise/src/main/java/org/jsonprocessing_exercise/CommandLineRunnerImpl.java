96e34d354d8fa8e35ca61a86151955a3d1f63c41
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-13 17:38:56 +0200

    Update CommandLineRunnerImpl.java

--- Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/CommandLineRunnerImpl.java
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/CommandLineRunnerImpl.java
@@ -1,19 +1,23 @@
 package org.jsonprocessing_exercise;
 
 import org.jsonprocessing_exercise.service.CategoryService;
+import org.jsonprocessing_exercise.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
     private final CategoryService categoryService;
+    private final UserService userService;
 
-    public CommandLineRunnerImpl(CategoryService categoryService) {
+    public CommandLineRunnerImpl(CategoryService categoryService, UserService userService) {
         this.categoryService = categoryService;
+        this.userService = userService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         this.categoryService.seedCategories();
+        this.userService.seedUsers();
     }
 }
