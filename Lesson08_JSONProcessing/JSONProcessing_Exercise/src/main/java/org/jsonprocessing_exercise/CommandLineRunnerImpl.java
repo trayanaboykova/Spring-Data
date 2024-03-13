91c62a2e9b79dbb8a50ae0a0e9637335068176a6
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-13 18:35:23 +0200

    updating

--- Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/CommandLineRunnerImpl.java
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/CommandLineRunnerImpl.java
@@ -1,6 +1,7 @@
 package org.jsonprocessing_exercise;
 
 import org.jsonprocessing_exercise.service.CategoryService;
+import org.jsonprocessing_exercise.service.ProductService;
 import org.jsonprocessing_exercise.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,15 +10,18 @@ import org.springframework.stereotype.Component;
 public class CommandLineRunnerImpl implements CommandLineRunner {
     private final CategoryService categoryService;
     private final UserService userService;
+    private final ProductService productService;
 
-    public CommandLineRunnerImpl(CategoryService categoryService, UserService userService) {
+    public CommandLineRunnerImpl(CategoryService categoryService, UserService userService, ProductService productService) {
         this.categoryService = categoryService;
         this.userService = userService;
+        this.productService = productService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         this.categoryService.seedCategories();
         this.userService.seedUsers();
+        this.productService.seedProducts();
     }
 }
