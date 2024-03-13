5804a5f0445b4186f1f14845a7d2931ce8641d5b
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-13 07:11:59 +0200

    Create Person.java

---
+++ Lesson08_JSONProcessing/JSONProcessing_Lab/src/main/java/bg/softuni/jsonprocessing_lab/entities/Person.java
@@ -0,0 +1,53 @@
+package bg.softuni.jsonprocessing_lab.entities;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private boolean isMarried;
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+
+    public void setMarried(boolean married) {
+        isMarried = married;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", isMarried=" + isMarried +
+                '}';
+    }
+}
