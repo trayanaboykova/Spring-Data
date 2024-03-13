ea4f805d316f9e138ca57c9de39f685bc6b05e10
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-13 10:53:06 +0200

    adding files

---
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/test/java/org/jsonprocessing_exercise/JsonProcessingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsonprocessing_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonProcessingExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
