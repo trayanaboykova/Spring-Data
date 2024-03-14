cbdf108aef3587e5b3b729c885f170ee16415649
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-14 11:02:24 +0200

    Update ProductService.java

--- Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/service/ProductService.java
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/service/ProductService.java
@@ -1,7 +1,13 @@
 package org.jsonprocessing_exercise.service;
 
+import org.jsonprocessing_exercise.service.dtos.ProductInRangeDto;
+
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface ProductService {
     void seedProducts() throws FileNotFoundException;
+    List<ProductInRangeDto> getAllProductsInRange(BigDecimal from, BigDecimal to);
+    void printAllProductsInRange(BigDecimal from, BigDecimal to);
 }
