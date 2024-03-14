80c9b8547847d338846010b4a2ca177a864e15dc
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-14 14:53:03 +0200

    Create CategoryByProductsDto.java

---
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/service/dtos/CategoryByProductsDto.java
@@ -0,0 +1,49 @@
+package org.jsonprocessing_exercise.service.dtos;
+
+import com.google.gson.annotations.Expose;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class CategoryByProductsDto implements Serializable {
+    @Expose
+    private String name;
+    @Expose
+    private int productsCount;
+    @Expose
+    private BigDecimal averagePrice;
+    @Expose
+    private BigDecimal totalRevenue;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getProductsCount() {
+        return productsCount;
+    }
+
+    public void setProductsCount(int productsCount) {
+        this.productsCount = productsCount;
+    }
+
+    public BigDecimal getAveragePrice() {
+        return averagePrice;
+    }
+
+    public void setAveragePrice(BigDecimal averagePrice) {
+        this.averagePrice = averagePrice;
+    }
+
+    public BigDecimal getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    public void setTotalRevenue(BigDecimal totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+}
