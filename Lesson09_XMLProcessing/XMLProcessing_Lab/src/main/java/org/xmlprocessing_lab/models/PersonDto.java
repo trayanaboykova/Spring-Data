06326364fafe282e709e0b8af44ff0161a26cc8c
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-14 18:53:33 +0200

    Update PersonDto.java

--- Lesson09_XMLProcessing/XMLProcessing_Lab/src/main/java/org/xmlprocessing_lab/models/PersonDto.java
+++ Lesson09_XMLProcessing/XMLProcessing_Lab/src/main/java/org/xmlprocessing_lab/models/PersonDto.java
@@ -1,9 +1,21 @@
 package org.xmlprocessing_lab.models;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class PersonDto {
+    @XmlElement(name = "firstName")
     private String firstName;
+    @XmlElement(name = "lastName")
     private String lastName;
+    @XmlElement(name = "age")
     private int age;
+
+    public PersonDto() {}
 
     public PersonDto(String firstName, String lastName, int age) {
         this.firstName = firstName;
