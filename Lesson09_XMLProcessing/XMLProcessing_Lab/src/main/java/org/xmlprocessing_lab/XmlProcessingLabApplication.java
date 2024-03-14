ccf4809713481ea88b7742e6bc8eb3f30fe2df31
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-14 17:12:44 +0200

    adding files
    xml processing lab inital project files

---
+++ Lesson09_XMLProcessing/XMLProcessing_Lab/src/main/java/org/xmlprocessing_lab/XmlProcessingLabApplication.java
@@ -0,0 +1,13 @@
+package org.xmlprocessing_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlProcessingLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlProcessingLabApplication.class, args);
+    }
+
+}
---
+++ Lesson09_XMLProcessing/XMLProcessing_Lab/src/test/java/org/xmlprocessing_lab/XmlProcessingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xmlprocessing_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlProcessingLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
