2f2c55546583c5f6b18d61ae1867a541171f347f
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-14 11:02:19 +0200

    Update ProductRepository.java

--- Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/data/repositories/ProductRepository.java
+++ Lesson08_JSONProcessing/JSONProcessing_Exercise/src/main/java/org/jsonprocessing_exercise/data/repositories/ProductRepository.java
@@ -4,7 +4,10 @@ import org.jsonprocessing_exercise.data.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.Set;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Set<Product> findAllByPriceBetweenAndBuyerIsNullOrderByPrice(BigDecimal from, BigDecimal to);
 }
