68b83b6f65fa0af045e703ab8e22eebda47f1134
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-16 13:14:47 +0200

    xml processing exercise
    creating xml processing exercise  project

---
+++ Lesson09_XMLProcessing/XMLProcessing_Exercise/src/main/java/org/xmlprocessing_exercise/XmlProcessingExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.xmlprocessing_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlProcessingExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlProcessingExerciseApplication.class, args);
+    }
+
+}
---
+++ Lesson09_XMLProcessing/XMLProcessing_Exercise/src/test/java/org/xmlprocessing_exercise/XmlProcessingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xmlprocessing_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlProcessingExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
