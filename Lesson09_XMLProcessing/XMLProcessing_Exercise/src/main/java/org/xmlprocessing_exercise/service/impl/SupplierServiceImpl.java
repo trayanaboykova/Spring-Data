d4335dce7a90a2cbc7eaf5ff2dd63ccdd79ae56c
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-17 00:35:47 +0200

    Update SupplierServiceImpl.java

--- Lesson09_XMLProcessing/XMLProcessing_Exercise/src/main/java/org/xmlprocessing_exercise/service/impl/SupplierServiceImpl.java
+++ Lesson09_XMLProcessing/XMLProcessing_Exercise/src/main/java/org/xmlprocessing_exercise/service/impl/SupplierServiceImpl.java
@@ -1,26 +1,30 @@
 package org.xmlprocessing_exercise.service.impl;
 
-import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Unmarshaller;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.xmlprocessing_exercise.data.entities.Supplier;
 import org.xmlprocessing_exercise.data.repositories.SupplierRepository;
 import org.xmlprocessing_exercise.service.SupplierService;
 import org.xmlprocessing_exercise.service.dto.imports.SupplierSeedDto;
 import org.xmlprocessing_exercise.service.dto.imports.SupplierSeedRootDto;
+import org.xmlprocessing_exercise.util.ValidationUtil;
 import org.xmlprocessing_exercise.util.XmlParser;
 
-import java.io.File;
 
 @Service
 public class SupplierServiceImpl implements SupplierService {
     private static final String FILE_IMPORT_PATH = "src/main/resources/xml/suppliers.xml";
     private final SupplierRepository supplierRepository;
     private final XmlParser xmlParser;
+    private final ValidationUtil validationUtil;
+    private final ModelMapper modelMapper;
 
-    public SupplierServiceImpl(SupplierRepository supplierRepository, XmlParser xmlParser) {
+    public SupplierServiceImpl(SupplierRepository supplierRepository, XmlParser xmlParser, ValidationUtil validationUtil, ModelMapper modelMapper) {
         this.supplierRepository = supplierRepository;
         this.xmlParser = xmlParser;
+        this.validationUtil = validationUtil;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -28,7 +32,12 @@ public class SupplierServiceImpl implements SupplierService {
         if (this.supplierRepository.count() == 0) {
             SupplierSeedRootDto supplierSeedRootDto = xmlParser.parse(SupplierSeedRootDto.class, FILE_IMPORT_PATH);
             for (SupplierSeedDto supplierSeedDto : supplierSeedRootDto.getSupplierSeedDtoList()) {
-
+                if (!this.validationUtil.isValid(supplierSeedDto)) {
+                    System.out.println("Invalid supplier data");
+                    continue;
+                }
+                Supplier supplier = this.modelMapper.map(supplierSeedDto, Supplier.class);
+                this.supplierRepository.saveAndFlush(supplier);
             }
         }
     }
