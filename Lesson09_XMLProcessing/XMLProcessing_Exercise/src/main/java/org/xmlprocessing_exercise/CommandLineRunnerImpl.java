ba6536a523461dfbd644bd080b81abfb31296611
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-17 00:35:40 +0200

    Update CommandLineRunnerImpl.java

--- Lesson09_XMLProcessing/XMLProcessing_Exercise/src/main/java/org/xmlprocessing_exercise/CommandLineRunnerImpl.java
+++ Lesson09_XMLProcessing/XMLProcessing_Exercise/src/main/java/org/xmlprocessing_exercise/CommandLineRunnerImpl.java
@@ -2,12 +2,18 @@ package org.xmlprocessing_exercise;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.xmlprocessing_exercise.service.SupplierService;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
+    private final SupplierService supplierService;
+
+    public CommandLineRunnerImpl(SupplierService supplierService) {
+        this.supplierService = supplierService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-
+        this.supplierService.seedSupplier();
     }
 }
