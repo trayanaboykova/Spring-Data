7fdadb373aec87575fc2a756a6af449c07c42097
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-16 17:53:19 +0200

    Update ApplicationBeanConfiguration.java

--- Lesson09_XMLProcessing/XMLProcessing_Exercise/src/main/java/org/xmlprocessing_exercise/config/ApplicationBeanConfiguration.java
+++ Lesson09_XMLProcessing/XMLProcessing_Exercise/src/main/java/org/xmlprocessing_exercise/config/ApplicationBeanConfiguration.java
@@ -1,7 +1,13 @@
 package org.xmlprocessing_exercise.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationBeanConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
