7d80a47e570622330c95add3b7e724f79827c312
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-17 13:25:33 +0200

    Create LocalDateTimeAdapter.java

---
+++ Lesson09_XMLProcessing/XMLProcessing_Exercise/src/main/java/org/xmlprocessing_exercise/util/adapters/LocalDateTimeAdapter.java
@@ -0,0 +1,17 @@
+package org.xmlprocessing_exercise.util.adapters;
+
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeAdapter  extends XmlAdapter<String, LocalDateTime> {
+    @Override
+    public LocalDateTime unmarshal(String s) throws Exception {
+        return LocalDateTime.parse(s);
+    }
+
+    @Override
+    public String marshal(LocalDateTime localDateTime) throws Exception {
+        return localDateTime.toString();
+    }
+}
