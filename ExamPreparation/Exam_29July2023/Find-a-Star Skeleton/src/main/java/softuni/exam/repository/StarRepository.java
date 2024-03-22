870f9e5241728cb5274796ce0b2dcfdec4cad6e7
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-22 19:03:06 +0200

    updating stars

--- ExamPreparation/Exam_29July2023/Find-a-Star Skeleton/src/main/java/softuni/exam/repository/StarRepository.java
+++ ExamPreparation/Exam_29July2023/Find-a-Star Skeleton/src/main/java/softuni/exam/repository/StarRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.models.entity.Star;
 
+import java.util.Optional;
+
 @Repository
 public interface StarRepository extends JpaRepository<Star, Long> {
+
+    Optional<Star> findByName(String name);
 
 }
--- ExamPreparation/Exam_29July2023/Find-a-Star Skeleton/src/main/java/softuni/exam/service/impl/StarServiceImpl.java
+++ ExamPreparation/Exam_29July2023/Find-a-Star Skeleton/src/main/java/softuni/exam/service/impl/StarServiceImpl.java
@@ -1,20 +1,36 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.jsons.StarSeedDto;
+import softuni.exam.models.entity.Star;
+import softuni.exam.models.entity.StarType;
+import softuni.exam.repository.ConstellationRepository;
 import softuni.exam.repository.StarRepository;
 import softuni.exam.service.StarService;
+import softuni.exam.util.ValidationUtil;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 @Service
 public class StarServiceImpl implements StarService {
     private static final String FILE_PATH = "src/main/resources/files/json/stars.json";
     private final StarRepository starRepository;
+    private final ConstellationRepository constellationRepository;
+    private final Gson gson;
+    private final ModelMapper modelMapper;
+    private final ValidationUtil validationUtil;
 
-    public StarServiceImpl(StarRepository starRepository) {
+    public StarServiceImpl(StarRepository starRepository, ConstellationRepository constellationRepository, Gson gson, ModelMapper modelMapper, ValidationUtil validationUtil) {
         this.starRepository = starRepository;
+        this.constellationRepository = constellationRepository;
+        this.gson = gson;
+        this.modelMapper = modelMapper;
+        this.validationUtil = validationUtil;
     }
 
     @Override
@@ -29,7 +45,24 @@ public class StarServiceImpl implements StarService {
 
     @Override
     public String importStars() throws IOException {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        StarSeedDto[] starSeedDtos = this.gson.fromJson(readStarsFileContent(), StarSeedDto[].class);
+
+        for (StarSeedDto starSeedDto : starSeedDtos) {
+            Optional<Star> optional = this.starRepository.findByName(starSeedDto.getName());
+            if (!this.validationUtil.isValid(starSeedDto) || optional.isPresent()) {
+                sb.append("Invalid star\n");
+                continue;
+            }
+
+            Star star = this.modelMapper.map(starSeedDto, Star.class);
+            star.setStarType(StarType.valueOf(starSeedDto.getStarType()));
+            star.setConstellation(this.constellationRepository.getById(starSeedDto.getConstellation()));
+            this.starRepository.saveAndFlush(star);
+            sb.append(String.format("Successfully imported star %s - %.2f light years\n", star.getName(), star.getLightYears()));
+        }
+
+        return sb.toString();
     }
 
     @Override
