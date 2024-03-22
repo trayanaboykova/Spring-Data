5d30433a69ba8ecdc42bbd6fb5bf4ccc8af19b69
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-22 15:59:58 +0200

    updating project

--- ExamPreparation/Exam_29July2023/Find-a-Star Skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ ExamPreparation/Exam_29July2023/Find-a-Star Skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -1,6 +1,13 @@
 package softuni.exam.config;
 
-// TODO:
-public class ApplicationBeanConfiguration {
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class ApplicationBeanConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
