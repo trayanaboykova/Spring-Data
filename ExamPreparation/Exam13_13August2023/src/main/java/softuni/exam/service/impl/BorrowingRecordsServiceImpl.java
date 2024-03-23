61c6f29b2aa524f29aadae7a1c6bdfc30759b2de
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-23 12:35:01 +0200

    update borrowing records service

--- ExamPreparation/Exam13_13August2023/src/main/java/softuni/exam/service/impl/BorrowingRecordsServiceImpl.java
+++ ExamPreparation/Exam13_13August2023/src/main/java/softuni/exam/service/impl/BorrowingRecordsServiceImpl.java
@@ -1,6 +1,30 @@
 package softuni.exam.service.impl;
 
-// TODO: Implement all methods
-public class BorrowingRecordsServiceImpl {
 
+import softuni.exam.service.BorrowingRecordsService;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+
+public class BorrowingRecordsServiceImpl implements BorrowingRecordsService {
+
+    @Override
+    public boolean areImported() {
+        return false;
+    }
+
+    @Override
+    public String readBorrowingRecordsFromFile() throws IOException {
+        return null;
+    }
+
+    @Override
+    public String importBorrowingRecords() throws IOException, JAXBException {
+        return null;
+    }
+
+    @Override
+    public String exportBorrowingRecords() {
+        return null;
+    }
 }
