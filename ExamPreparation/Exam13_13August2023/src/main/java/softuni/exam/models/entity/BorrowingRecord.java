a1dc91518b49cc631c5cb057f1701fe405f48829
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-23 12:31:29 +0200

    Update BorrowingRecord.java

--- ExamPreparation/Exam13_13August2023/src/main/java/softuni/exam/models/entity/BorrowingRecord.java
+++ ExamPreparation/Exam13_13August2023/src/main/java/softuni/exam/models/entity/BorrowingRecord.java
@@ -1,4 +1,22 @@
 package softuni.exam.models.entity;
 
-public class BorrowingRecord {
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "borrowing_records")
+public class BorrowingRecord extends BaseEntity {
+    @Column(name = "borrow_date")
+    private LocalDate borrowDate;
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+    @Column
+    private String remarks;
+    @ManyToOne
+    @JoinColumn(name = "books_id", referencedColumnName = "id")
+    private Book books;
+    @ManyToOne
+    @JoinColumn(name = "member_id", referencedColumnName = "id")
+    private LibraryMember libraryMember;
+
 }
