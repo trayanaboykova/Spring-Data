eee93588d6c362ca91146fa449f85db87588a0d4
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-23 12:35:29 +0200

    update library member service

--- ExamPreparation/Exam13_13August2023/src/main/java/softuni/exam/service/impl/LibraryMemberServiceImpl.java
+++ ExamPreparation/Exam13_13August2023/src/main/java/softuni/exam/service/impl/LibraryMemberServiceImpl.java
@@ -1,6 +1,24 @@
 package softuni.exam.service.impl;
 
-// TODO: Implement all methods
-public class LibraryMemberServiceImpl{
 
+import softuni.exam.service.LibraryMemberService;
+
+import java.io.IOException;
+
+public class LibraryMemberServiceImpl implements LibraryMemberService {
+
+    @Override
+    public boolean areImported() {
+        return false;
+    }
+
+    @Override
+    public String readLibraryMembersFileContent() throws IOException {
+        return null;
+    }
+
+    @Override
+    public String importLibraryMembers() throws IOException {
+        return null;
+    }
 }
