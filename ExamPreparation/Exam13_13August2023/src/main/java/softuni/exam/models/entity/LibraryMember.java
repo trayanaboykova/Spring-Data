be594b3a7573347b1e65ac3483275584b13333f1
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-23 12:09:42 +0200

    Update LibraryMember.java

--- ExamPreparation/Exam13_13August2023/src/main/java/softuni/exam/models/entity/LibraryMember.java
+++ ExamPreparation/Exam13_13August2023/src/main/java/softuni/exam/models/entity/LibraryMember.java
@@ -1,4 +1,18 @@
 package softuni.exam.models.entity;
 
-public class LibraryMember {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "library_members")
+public class LibraryMember extends BaseEntity {
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column
+    private String address;
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
 }
