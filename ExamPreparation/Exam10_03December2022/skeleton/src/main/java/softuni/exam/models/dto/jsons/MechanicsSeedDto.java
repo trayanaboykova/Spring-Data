214488ad5428440bf99a0f263edb8852eab33fbf
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-28 14:20:25 +0200

    Update MechanicsSeedDto.java

--- ExamPreparation/Exam10_03December2022/skeleton/src/main/java/softuni/exam/models/dto/jsons/MechanicsSeedDto.java
+++ ExamPreparation/Exam10_03December2022/skeleton/src/main/java/softuni/exam/models/dto/jsons/MechanicsSeedDto.java
@@ -1,4 +1,62 @@
 package softuni.exam.models.dto.jsons;
 
-public class MechanicsSeedDto {
+import com.google.gson.annotations.Expose;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+public class MechanicsSeedDto implements Serializable {
+    @Expose
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    private String email;
+    @Expose
+    @NotNull
+    @Size(min = 2)
+    private String firstName;
+    @Expose
+    @NotNull
+    @Size(min = 2)
+    private String lastName;
+    @Expose
+    @Size(min = 2)
+    private String phone;
+
+    public MechanicsSeedDto() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
