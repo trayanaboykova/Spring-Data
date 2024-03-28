82e5ecef868636a6260e07ab3b2ad25fc5f83969
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-28 17:34:12 +0200

    Update CarRootDto.java

--- ExamPreparation/Exam10_03December2022/skeleton/src/main/java/softuni/exam/models/dto/xmls/CarRootDto.java
+++ ExamPreparation/Exam10_03December2022/skeleton/src/main/java/softuni/exam/models/dto/xmls/CarRootDto.java
@@ -2,6 +2,7 @@ package softuni.exam.models.dto.xmls;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.List;
 @XmlRootElement(name = "cars")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CarRootDto implements Serializable {
+    @XmlElement(name = "car")
     private List<CarSeedDto> cars = new ArrayList<>();
 
     public CarRootDto() {
