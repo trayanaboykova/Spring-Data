db94e4d3d370416fb8cfb51919f1a96f07ea2f36
Trayana Boykova <trayana.boykova@gmail.com>
2024-03-28 11:27:05 +0200

    Update LocalDateTimeAdapter.java

--- ExamPreparation/Exam10_03December2022/skeleton/src/main/java/softuni/exam/util/LocalDateTimeAdapter.java
+++ ExamPreparation/Exam10_03December2022/skeleton/src/main/java/softuni/exam/util/LocalDateTimeAdapter.java
@@ -1,4 +1,20 @@
 package softuni.exam.util;
 
-public class LocalDateTimeAdapter {
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeAdapter  extends XmlAdapter<String, LocalDateTime> {
+
+    @Override
+    public LocalDateTime unmarshal(String dateTimeFromXML) throws Exception {
+
+        final String dateTime = dateTimeFromXML.trim().replaceAll(" ", "T");
+
+        return LocalDateTime.parse(dateTime);
+    }
+
+    @Override
+    public String marshal(LocalDateTime dateTime) throws Exception {
+        return dateTime.toString();
+    }
 }
