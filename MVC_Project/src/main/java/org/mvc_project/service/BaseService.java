5a2f601b2cdba338a7cecbdf774423656264c141
Trayana Boykova <trayana.boykova@gmail.com>
2025-01-14 15:47:31 +0200

    Update BaseService.java

--- MVC_Project/src/main/java/org/mvc_project/service/BaseService.java
+++ MVC_Project/src/main/java/org/mvc_project/service/BaseService.java
@@ -1,4 +1,13 @@
 package org.mvc_project.service;
 
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+
 public interface BaseService {
+    boolean isImported();
+
+    void seedData() throws JAXBException, IOException;
+
+    String readFile() throws IOException;
 }
