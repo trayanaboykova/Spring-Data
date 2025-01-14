a7d01f87fed8a9eadad17c0e8cfe5758b7502d03
Trayana Boykova <trayana.boykova@gmail.com>
2025-01-14 12:09:31 +0200

    creating mvc project

---
+++ MVC_Project/src/test/java/org/mvc_project/MvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mvc_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
