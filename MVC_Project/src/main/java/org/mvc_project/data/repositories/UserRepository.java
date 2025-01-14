03e0602bf7212cc0370fa4ded8e94c23111c4de5
Trayana Boykova <trayana.boykova@gmail.com>
2025-01-14 15:47:28 +0200

    updating user

--- MVC_Project/src/main/java/org/mvc_project/data/repositories/UserRepository.java
+++ MVC_Project/src/main/java/org/mvc_project/data/repositories/UserRepository.java
@@ -4,7 +4,13 @@ import org.mvc_project.data.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
--- MVC_Project/src/main/java/org/mvc_project/service/UserService.java
+++ MVC_Project/src/main/java/org/mvc_project/service/UserService.java
@@ -1,5 +1,12 @@
 package org.mvc_project.service;
 
-public interface UserService {
+import org.mvc_project.web.models.UserLoginModel;
+import org.mvc_project.web.models.UserRegisterModel;
 
+public interface UserService {
+    boolean validateRegisterModel(UserRegisterModel userRegisterModel);
+
+    void registerUser(UserRegisterModel userRegisterModel);
+
+    boolean loggedIn(UserLoginModel userLoginModel);
 }
